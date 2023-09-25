ee49c32b52d72bd4b129684ea88c3b8244a3cc82
hitemmup <amwolverine2.0@gmail.com>
2023-09-26 01:00:32 +0530

    landing Page content updated

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,8 +1,8 @@
-import styled from 'styled-components';
-import Wrapper from '../assets/wrappers/LandingPage';
-import main from '../assets/images/main.svg';
-import { Link } from 'react-router-dom';
-import { Logo } from '../components';
+import styled from 'styled-components'
+import Wrapper from '../assets/wrappers/LandingPage'
+import main from '../assets/images/main.svg'
+import { Link } from 'react-router-dom'
+import { Logo } from '../components'
 
 const Landing = () => {
   return (
@@ -10,28 +10,27 @@ const Landing = () => {
       <nav>
         <Logo />
       </nav>
-      <div className='container page'>
-        <div className='info'>
+      <div className="container page">
+        <div className="info">
           <h1>
             job <span>tracking</span> app
           </h1>
           <p>
-            I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
-            bottle single-origin coffee chia. Aesthetic post-ironic venmo,
-            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
-            narwhal.
+            JobChronicle: Your Personal Job Application Organizer - Effortlessly
+            track and manage all your job applications in one place, making your
+            job search journey organized and stress-free.
           </p>
-          <Link to='/register' className='btn register-link'>
+          <Link to="/register" className="btn register-link">
             Register
           </Link>
-          <Link to='/login' className='btn '>
+          <Link to="/login" className="btn ">
             Login / Demo User
           </Link>
         </div>
-        <img src={main} alt='job hunt' className='img main-img' />
+        <img src={main} alt="job hunt" className="img main-img" />
       </div>
     </Wrapper>
-  );
-};
+  )
+}
 
-export default Landing;
+export default Landing
